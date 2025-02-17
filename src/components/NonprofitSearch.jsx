26fa957f43b5bc5e2631b8b1ba48436bcd04e90e
Sandeep thakur <katwal.sandeep28@gmail.com>
2025-02-17 15:42:59 +0530

    added search screen

--- src/components/NonprofitSearch.jsx
+++ src/components/NonprofitSearch.jsx
@@ -1,69 +1,136 @@
-import { MapPin, UserRound } from 'lucide-react'
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { MapPin, UserRound } from "lucide-react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const crwds = [
-    {
-title:'Nonprofits Heading',
-desc:'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
-image:'np1.svg',
-location:'New York',
-members:45
-    },
-    {
-        title:'Nonprofits Heading',
-        desc:'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
-        image:'np2.svg',
-        location:'Paris',
-        members:45
-    },
-    {
-        title:'Nonprofits Heading',
-        desc:'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
-        image:'np3.svg',
-        location:'Nigeria',
-        members:40
-    }
-]
+  {
+    title: "Nonprofits Heading",
+    desc: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam",
+    image: "np1.svg",
+    location: "New York",
+    members: 45,
+  },
+  {
+    title: "Nonprofits Heading",
+    desc: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam",
+    image: "np2.svg",
+    location: "Paris",
+    members: 45,
+  },
+  {
+    title: "Nonprofits Heading",
+    desc: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam",
+    image: "np3.svg",
+    location: "Nigeria",
+    members: 40,
+  },
+];
 
-const NonProfitSearch = ({item}) => {
+const NonProfitSearch = ({ item, isTyping, type }) => {
   const navigation = useNavigate();
 
-  const navigateNonprofit = ()=>{
-        navigation('/nonprofit');
-  }
+  const navigateNonprofit = () => {
+    navigation("/nonprofit");
+  };
+
+  if (isTyping)
+    return (
+      <div className="flex flex-col items-center justify-center text-center p-4">
+        <h4 className="text-xl font-semibold mb-2">
+          Hmm, no results for that search.
+        </h4>
+        <p className="mb-4">Please try again</p>
+        <img
+          src={notFound || "/placeholder.svg"}
+          alt="Not Found"
+          className="max-w-full h-auto"
+        />
+      </div>
+    );
+
   return (
-    <div className='rounded-xl p-4 flex flex-col xl:flex-row   gap-5  bg-[#f3f2f2]'>
-        <img src={item.image} className='rounded-xl w-full h-[200px]    xl:w-[304px] xl:h-[260px] object-right-top object-cover '/>
-        <div className='flex flex-col gap-2  items-start'>
-        <div className="space-y-2">
-            <h3 className="text-xl md:text-2xl font-semibold text-gray-900">{item.title}</h3>
-            <p className="text-[12px] md:text-[14px]  text-gray-600">{item.desc}</p>
+    <>
+      {type === "nonprofits" ? (
+        <div className="rounded-xl p-4 flex flex-col xl:flex-row   gap-5  bg-[#f3f2f2]">
+          <img
+            src={item.image}
+            className="rounded-xl w-full h-[200px]    xl:w-[304px] xl:h-[260px] object-right-top object-cover "
+          />
+          <div className="flex flex-col gap-2  items-start">
+            <div className="space-y-2">
+              <h3 className="text-xl md:text-2xl font-semibold text-gray-900">
+                {item.title}
+              </h3>
+              <p className="text-[12px] md:text-[14px]  text-gray-600">
+                {item.desc}
+              </p>
+            </div>
+            <div className="flex flex-row items-center gap-5 my-4">
+              <div className="flex flex-row items-center gap-2">
+                <MapPin className="h-3 w-3 md:h-6 md:w-6" />
+                <div className="text-[12px] md:text-[16px]">
+                  {item.location}
+                </div>
+              </div>
+
+              <div className="flex flex-row items-center gap-2">
+                <img src="people.svg" className="h-3 w-3 md:h-6 md:w-6" />
+                <div className="text-[12px] md:text-[16px]">
+                  {item.members} Members
+                </div>
+              </div>
+            </div>
+            <button
+              onClick={navigateNonprofit}
+              className="text-[12px] md:text-[14px] px-4 py-2 md:px-8 md:py-2 bg-[#393939] hover:shadow-xl transition-shadow cursor-pointer rounded-full border border-gray-300 text-white font-medium"
+            >
+              Donate Now
+            </button>
+          </div>
         </div>
-        <div className='flex flex-row items-center gap-5 my-4'>
-            <div className='flex flex-row items-center gap-2'>
-            <MapPin className="h-3 w-3 md:h-6 md:w-6" />
-            <div className="text-[12px] md:text-[16px]">
-            {item.location}
-            </div>
-            </div>
+      ) : (
+        <>
+          <div className="rounded-xl p-4 flex flex-col xl:flex-row   gap-5  bg-[#f3f2f2]">
+            <img
+              src="/crwdimg.png"
+              className="rounded-xl w-full h-[200px]    xl:w-[304px] xl:h-[260px] object-right-top object-cover "
+            />
+            <div className="flex flex-col gap-2  items-start">
+              <div className="space-y-2">
+                <h3 className="text-xl md:text-2xl font-semibold text-gray-900">
+                  {item.crwdHeading}
+                </h3>
+                <p className="text-[12px] md:text-[14px]  text-gray-600">
+                  {item.crwdDesc}
+                </p>
+              </div>
+              <div className="flex flex-row items-center gap-5 my-4">
+                {/* <div className="flex flex-row items-center gap-2">
+                  <MapPin className="h-3 w-3 md:h-6 md:w-6" />
+                  <div className="text-[12px] md:text-[16px]">
+                    {item.location}
+                  </div>
+                </div> */}
 
-            <div className='flex flex-row items-center gap-2'>
-            <img src='people.svg' className="h-3 w-3 md:h-6 md:w-6"/>
-            <div  className="text-[12px] md:text-[16px]">
-            {item.members} Members
+                <div className="flex flex-row items-center gap-2">
+                  <img src="people.svg" className="h-3 w-3 md:h-6 md:w-6" />
+                  <div className="text-[12px] md:text-[16px]">
+                    {item.members} Members
+                  </div>
+                </div>
+              </div>
+              <button
+                onClick={navigateNonprofit}
+                className="text-[12px] md:text-[14px] px-4 py-2 md:px-8 md:py-2 bg-[#393939] hover:shadow-xl transition-shadow cursor-pointer rounded-full border border-gray-300 text-white font-medium"
+              >
+                Donate Now
+              </button>
             </div>
-            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
 
-        </div>
-        <button onClick={navigateNonprofit} className="text-[12px] md:text-[14px] px-4 py-2 md:px-8 md:py-2 bg-[#393939] hover:shadow-xl transition-shadow cursor-pointer rounded-full border border-gray-300 text-white font-medium">
-          Donate Now
-        </button>
-
-        </div>
-      
-    </div>
-  )
-}
-
-export default NonProfitSearch
+export default NonProfitSearch;
--- src/screens/Search.jsx
+++ src/screens/Search.jsx
@@ -1,75 +1,89 @@
-import React, { useEffect, useState } from 'react'
-import {  Search } from "lucide-react"
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import NonProfitSearch from '../components/NonprofitSearch'
-
-
+import React, { useEffect, useState } from "react";
+import { Search } from "lucide-react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import NonProfitSearch from "../components/NonprofitSearch";
 
 const crwds = [
-    {
-title:'Nonprofits Heading',
-desc:'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
-image:'np1.svg',
-location:'New York',
-members:45
-    },
-    {
-        title:'Nonprofits Heading',
-        desc:'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
-        image:'np2.svg',
-        location:'Paris',
-        members:45
-    },
-    {
-        title:'Nonprofits Heading',
-        desc:'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
-        image:'np3.svg',
-        location:'Nigeria',
-        members:40
-    }
-]
+  {
+    title: "Nonprofits Heading",
+    desc: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam",
+    image: "np1.svg",
+    location: "New York",
+    members: 45,
+    crwdHeading: "CRWDs Group heading here ",
+    crwdDesc:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam",
+  },
+  {
+    title: "Nonprofits Heading",
+    desc: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam",
+    image: "np2.svg",
+    location: "Paris",
+    members: 45,
+    crwdHeading: "CRWDs Group heading here ",
+    crwdDesc:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam",
+  },
+  {
+    title: "Nonprofits Heading",
+    desc: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam",
+    image: "np3.svg",
+    location: "Nigeria",
+    members: 40,
+    crwdHeading: "CRWDs Group heading here ",
+    crwdDesc:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam",
+  },
+];
 
 const SearchScreen = () => {
-    const [type,setType] = useState('crwd');
-    const [placeholder, setPlaceholder] = useState('Search');
+  const [type, setType] = useState("crwd");
+  const [placeholder, setPlaceholder] = useState("Search");
+  const [text, setText] = useState("");
 
-    useEffect(() => {
-      const handleResize = () => {
-        if (window.innerWidth < 640) { // Tailwind's `sm` breakpoint is 640px
-          setPlaceholder('Search');
-        } else {
-          setPlaceholder('Search for CRWDs and Nonprofits');
-        }
-      };
-  
-      // Set initial placeholder
-      handleResize();
-  
-      // Add event listener for window resize
-      window.addEventListener('resize', handleResize);
-  
-      // Cleanup
-      return () => window.removeEventListener('resize', handleResize);
-    }, []);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 640) {
+        // Tailwind's `sm` breakpoint is 640px
+        setPlaceholder("Search");
+      } else {
+        setPlaceholder("Search for CRWDs and Nonprofits");
+      }
+    };
+
+    // Set initial placeholder
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
-    <div >
-        <Header/>
-        <div className='flex p-5 md:p-15   xl:p-25  mt-[70px]  flex-col gap-5 justify-center    items-center'>
-        <div className="text-2xl   text-center md:text-5xl   font-bold text-[#393939] mb-5">Find CRWD and Nonprofits</div>
-        <div className='flex flex-row gap-3 md:p-2  p-1 w-full    md:w-[90%] lg:w-[60%] xl:w-[40%]  rounded-full border border=[#393939]'>
-            <div className='bg-[#393939] p-3 rounded-full   '>
-            <Search color="#fff" className="h-5 w-5 md:h-6 md:w-6"  />
-            </div>
-            <input
-  className="w-full text-lg md:text-2xl outline-none"
-  placeholder={placeholder}
-  // The zero-width space ensures the first part is always visible
-/>
+    <div>
+      <Header />
+      <div className="flex p-5 md:p-15   xl:p-25  mt-[70px]  flex-col gap-5 justify-center    items-center">
+        <div className="text-2xl   text-center md:text-5xl   font-bold text-[#393939] mb-5">
+          Find CRWD and Nonprofits
+        </div>
+        <div className="flex flex-row gap-3 md:p-2  p-1 w-full    md:w-[90%] lg:w-[60%] xl:w-[40%]  rounded-full border border=[#393939]">
+          <div className="bg-[#393939] p-3 rounded-full   ">
+            <Search color="#fff" className="h-5 w-5 md:h-6 md:w-6" />
+          </div>
+          <input
+            onChange={(e) => setText(e.target.value)}
+            className="w-full text-lg md:text-2xl outline-none"
+            placeholder={placeholder}
+            // The zero-width space ensures the first part is always visible
+          />
         </div>
 
-        <div className='flex flex-row justify-center mt-10 md:mt-20 items-center gap-4'>
-        <button onClick={()=>setType('crwd')}       className={`
+        <div className="flex flex-row justify-center mt-10 md:mt-20 items-center gap-4">
+          <button
+            onClick={() => setType("crwd")}
+            className={`
         px-8 flex-1 py-3 
         text-[14px]
         text-nowrap
@@ -81,17 +95,18 @@ const SearchScreen = () => {
         transition-shadow 
         cursor-pointer 
         font-semibold
-        ${type === 'crwd' 
-          ? 'bg-[#393939] text-white border-gray-300' 
-          : 'bg-white text-gray-700 border-gray-300'
+        ${
+          type === "crwd"
+            ? "bg-[#393939] text-white border-gray-300"
+            : "bg-white text-gray-700 border-gray-300"
         }
       `}
-    >
-         
-         
-          CRWD
-        </button>
-        <button onClick={()=>setType('nonprofits')}       className={`
+          >
+            CRWD
+          </button>
+          <button
+            onClick={() => setType("nonprofits")}
+            className={`
         px-8 flex-1 py-3 
         text-[14px]
         md:text-xl
@@ -101,40 +116,60 @@ const SearchScreen = () => {
         transition-shadow 
         cursor-pointer 
         font-semibold
-        ${type === 'nonprofits' 
-          ? 'bg-[#393939] text-white border-gray-300' 
-          : 'bg-white text-gray-700 border-gray-300'
+        ${
+          type === "nonprofits"
+            ? "bg-[#393939] text-white border-gray-300"
+            : "bg-white text-gray-700 border-gray-300"
         }
       `}
-    >
-          Nonprofits
-        </button>
-         </div>
-
-         <div className='flex flex-col w-full   gap-3 mt-10 md:mt-15'>
-           <div className="text-2xl   md:text-5xl  font-bold text-[#393939] ">CRWDs</div>
-           <div className="text-sm   md:text-lg  font-light text-[#393939] w-[85%] md:w-[60%] "> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.</div>
-         </div>
-
-
-         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        {
-            crwds.map((item,index)=>{
-              return   <NonProfitSearch key={index} item={item}/>
-            })
-        }
+          >
+            Nonprofits
+          </button>
+        </div>
+        <div className="flex flex-col w-full   gap-3 mt-10 md:mt-15">
+          <div className="text-2xl   md:text-5xl  font-bold text-[#393939] ">
+            {type === "crwd" ? "CRWDs" : "Nonprofits"}
+          </div>
+          <div className="text-sm   md:text-lg  font-light text-[#393939] w-[85%] md:w-[60%] ">
+            {" "}
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae.
+          </div>
+        </div>
+        {text?.length > 5 ? (
+          <div className="flex flex-col items-center justify-center text-center p-4">
+            <h4 className="text-xl font-semibold mb-2">
+              Hmm, no results for that search.
+            </h4>
+            <p className="mb-4 text-2xl font-normal text-[#616161]">
+              Please try again.
+            </p>
+            <img
+              src={"/placeholder.png"}
+              alt="Not Found"
+              className="max-w-full h-auto"
+            />
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+            {crwds.map((item, index) => {
+              return (
+                <NonProfitSearch
+                  key={index}
+                  isTyping={text?.length > 5}
+                  type={type}
+                  item={item}
+                />
+              );
+            })}
+          </div>
+        )}
       </div>
 
-        </div>
-
-
-
-
-
-
-      <Footer/>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default SearchScreen
+export default SearchScreen;
