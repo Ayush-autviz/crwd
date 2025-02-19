1e6ebe378f35990e2f66c581bd16a9525e7395bc
ayush <ayush.bajaj.98178@gmail.com>
2025-02-19 11:26:35 +0530

    1234

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
          </div>
           <div className="hidden md:block text-[20px]">Search</div>
         </div>
-        <img onClick={()=>{navigation('/')}}  src="crwd.png" alt="Logo" className="h-8 md:ml-15" />
+        <img onClick={()=>{navigation('/')}}  src="crwd.png" alt="Logo" className="h-8 md:ml-15 cursor-pointer " />
         <div className="flex flex-row items-center gap-4 justify-center">
           <div onClick={()=>{navigation('/login')}} className="px-8 py-2 cursor-pointer border rounded-full hidden md:block hover:shadow-xl transition-shadow ">Sign in</div>
           <button onClick={toggleSidebar} className=" h-10 w-10 hover:bg-[#F3F3F2] cursor-pointer justify-center items-center flex rounded-full">
